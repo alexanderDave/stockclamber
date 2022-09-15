612e916ba0206e4f01d169e15ab367a8578ed210
unknown <wei.dai@sh.energy-blockchain.com>
2022-09-15 15:00:32 +0800

    new add

--- ADO/dates.py
+++ ADO/dates.py
@@ -5,6 +5,8 @@ import utiltool as tool
 from DB import DB
 import akshare as ak
 import requests
+from qiyechatutil import Weichat
+import mplfinance as mpf
 
 class stockdates():
 
@@ -26,6 +28,40 @@ class stockdates():
         sql = f"insert into stockdata (st_name, pickledate,dt_create) values ('{name}',\"{res}\",'{dt}');"
 
         tool.save2db(sql)
+
+    def genFinance(self):
+        respath = config().res_path
+        filename = os.path.join(respath, '601601_20220.pickle')
+        pk = loadpickle(filename)
+        pk['日期'] = pd.to_datetime(pk['日期'])
+        pk.set_index(['日期'], inplace=True)
+        pk.index.set_names(['Date'], inplace=True)
+
+        pk.rename(columns={'开盘': 'Open', '收盘': 'Close', '最高': 'High', '最低': 'Low'}, inplace=True)
+        pk = pk.iloc[:, :4]
+        print(pk)
+        upboundDC = pd.Series(0.0, index=pk.Close.index)
+        downboundDC = pd.Series(0.0, index=pk.Close.index)
+        midboundDC = pd.Series(0.0, index=pk.Close.index)
+        rate = 0.4
+        for _ in range(20, len(pk.Close)):
+            upboundDC[_] = max(pk.High[(_ - 20):_])
+            downboundDC[_] = min(pk.Low[(_ - 20):_])
+            # midboundDC[_] = 0.382 * (upboundDC[_] + downboundDC[_])
+            midboundDC[_] = (upboundDC[_] - downboundDC[_]) * rate + downboundDC[_]
+
+        pk['upboundDC'] = upboundDC
+        pk['downboundDC'] = downboundDC
+        pk['midboundDC'] = midboundDC
+        plotdate = pk.iloc[-60:, :]
+
+        add_plot = [mpf.make_addplot(plotdate['upboundDC']),
+                    mpf.make_addplot(plotdate['downboundDC']),
+                    mpf.make_addplot(plotdate['midboundDC'])]
+
+        picname = filename.replace('pickle', 'png')
+        mpf.plot(plotdate, type='candle', addplot=add_plot, savefig=picname, title='demotest', mav=5)
+        Weichat().sendPics(picname)
 
 
 
---
+++ jobs/drawpics.py
@@ -0,0 +1,13 @@
+import os, sys
+root_path = os.path.dirname(os.path.dirname(__file__))
+ado_path = os.path.join(root_path, 'ADO')
+sys.path.append(root_path)
+sys.path.append(ado_path)
+from config import config
+from dates import stockdates
+
+if __name__ == '__main__':
+    codes = config().watchlist
+
+    for key,value in codes.items():
+        st = stockdates(key)
