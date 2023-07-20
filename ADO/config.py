67975d73c18effac052b6d900f5404481a800518
shanshan.chu <shanshan.chu@nenglian.com>
2023-07-20 15:51:52 +0800

    update codes

--- ADO/config.py
+++ ADO/config.py
@@ -7,12 +7,12 @@ class config():
         self.res_path = os.path.join(self.root_Path, 'resource')
         self.plot_path = os.path.join(self.root_Path, 'plot')
         self.mysqlinfo = ('127.0.0.1', 3306, 'root', '2021@lskj')
-        self.watchlist = {'300081':'HengXinDF','300364':'ZhongWenZX','600031':'SanYiZG','300014':'YiWeiLN',
-                          '601601':'ZhonGuoTB','601066':'ZhongXinJT','300059':'DongFCaiFu',
-                          '300999':'JingLongYu','002007':'HuaLanSW','002833':'HongYaShuKong',
-                          '300058':'LanSeGuangB','301091':'ShenChenJiao','300031':'BaoTongKeJi',
+        self.watchlist = {'300081':'HengXinDF','300364':'ZhongWenZX','600031':'SanYiZG',
+                          '300014':'YiWeiLN','002007':'HuaLanSW','000625':'changanQC',
+                          '601066':'ZhongXinJT','300059':'DongFCaiFu', '300999':'JingLongYu',
+                          '300058':'LanSeGuangB','301091':'ShenChenJiao','601888':'ZGzm',
                           '300223':'BeiJingJZ','002497':'YaHuaJiTUan','300581':'ChenXiHangKong',
-                          '300045': 'HuaLiChuangTong', '300088': 'ChangXinKeJi', '002409': 'YaKeKeJi',
+                          '300088': 'ChangXinKeJi', '002409': 'YaKeKeJi',
                           }
 
 
